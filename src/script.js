25503136054367bc6dabceb303f38950e84fa40f
Sorumond <sorumond@gmail.com>
2019-04-12 14:15:18 +0300

    test

--- src/script.js
+++ src/script.js
@@ -1,5 +1,25 @@
 $('.twitter').slick({
-  lazyLoad: 'ondemand',
-  slidesToShow: 5,
-  slidesToScroll: 1
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
